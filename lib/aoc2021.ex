59475a96d47f7bb31a17ad216c3ebe814b466618
Mikhail Aksenov <thecoldwine@gmail.com>
2021-12-02 11:16:21 +0100

    First day completed

--- lib/aoc2021.ex
+++ lib/aoc2021.ex
@@ -1,5 +1,29 @@
 defmodule Aoc2021 do
-  def day1() do
-    Aoc2021.Day1.run("aoc2021_test.txt")
+  defmodule Day1 do
+    def count(filepath) do
+      File.stream!(filepath)
+      |> Stream.map(fn e -> Integer.parse(e) |> elem(0) end)
+      |> Enum.reduce({nil, 0}, fn e, {prev, total} ->
+        cond do
+          prev < e -> {e, total + 1}
+          true -> {e, total}
+        end
+      end)
+    end
+
+    def window_count(filepath) do
+      File.stream!(filepath)
+      |> Stream.map(fn e -> Integer.parse(e) |> elem(0) end)
+      |> Stream.transform([nil, nil, nil], fn e, [_, b, c] -> {[b, c, e], [b, c, e]} end)
+      |> Stream.drop(6)
+      |> Stream.chunk_every(3)
+      |> Stream.map(&Enum.sum/1)
+      |> Enum.reduce({nil, 0}, fn e, {prev, total} ->
+        cond do
+          prev < e -> {e, total + 1}
+          true -> {e, total}
+        end
+      end)
+    end
   end
 end
