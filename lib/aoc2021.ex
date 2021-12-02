f1d49ee494108646a2b6788663cd45b85e39d117
Mikhail Aksenov <thecoldwine@gmail.com>
2021-12-02 11:21:31 +0100

    refactoring

--- lib/aoc2021.ex
+++ lib/aoc2021.ex
@@ -1,8 +1,16 @@
 defmodule Aoc2021 do
   defmodule Day1 do
-    def count(filepath) do
+    def run1_1(filepath), do: to_integer_stream(filepath) |> count()
+
+    def run1_2(filepath), do: to_integer_stream(filepath) |> window_count()
+
+    defp to_integer_stream(filepath) do
       File.stream!(filepath)
       |> Stream.map(fn e -> Integer.parse(e) |> elem(0) end)
+    end
+
+    def count(stream) do
+      stream
       |> Enum.reduce({nil, 0}, fn e, {prev, total} ->
         cond do
           prev < e -> {e, total + 1}
@@ -11,19 +19,13 @@ defmodule Aoc2021 do
       end)
     end
 
-    def window_count(filepath) do
-      File.stream!(filepath)
-      |> Stream.map(fn e -> Integer.parse(e) |> elem(0) end)
+    def window_count(stream) do
+      stream
       |> Stream.transform([nil, nil, nil], fn e, [_, b, c] -> {[b, c, e], [b, c, e]} end)
       |> Stream.drop(6)
       |> Stream.chunk_every(3)
       |> Stream.map(&Enum.sum/1)
-      |> Enum.reduce({nil, 0}, fn e, {prev, total} ->
-        cond do
-          prev < e -> {e, total + 1}
-          true -> {e, total}
-        end
-      end)
+      |> count
     end
   end
 end
